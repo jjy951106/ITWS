af0a74fbb3ba05086c8b2c8d5aa4015b12b4a0fd
jjy951106 <37281676+jjy951106@users.noreply.github.com>
2020-11-13 11:00:39 +0900

    test
    test

--- recvfrom_server.c
+++ recvfrom_server.c
@@ -23,6 +23,8 @@
 #define MEDIUM_TERM_SEC 0
 #define MEDIUM_TERM_NSEC 10000000 // nanosecond between receive and transmit
 
+#define DEFAULT_BUFLEN 512
+
 struct timespec s = { MEDIUM_TERM_SEC, MEDIUM_TERM_NSEC }; // time_t (long) sec long nsec
 
 #define PORT 5005 // default port
@@ -32,7 +34,6 @@ int Thread_t = 0; // total thread number played
 typedef struct udp_thread_factor{
     
     int sock;
-    struct msghdr msg;
     struct sockaddr_in from_addr;
 
 }udp_thread_factor;
@@ -42,34 +43,11 @@ static void err(const char *error){
     exit(1);
 }
 
-int recv_socket(int sock, struct msghdr *msg, struct sockaddr_in *from_addr){
-
-    /* recvpacket */
-    char data[256];
-    struct iovec entry;
-    struct {
-        struct cmsghdr cm;
-        char control[512];
-    } control;
-    memset(msg, 0, sizeof(*msg));
-    msg->msg_iov = &entry;
-    msg->msg_iovlen = 1;
-    entry.iov_base = data;
-    entry.iov_len = sizeof(data);
-    msg->msg_name = (caddr_t)from_addr;
-    msg->msg_namelen = sizeof(*from_addr);
-    msg->msg_control = &control;
-    msg->msg_controllen = sizeof(control);
-
-    return recvmsg(sock, msg, 0);
-
-}
-
+/*
 void *Server_Socket_Thread(void *arg){
 
     struct msghdr msg;
 
-    /* printpacket */
     struct cmsghdr *cm;
 
     int close_, n = 0;
@@ -111,7 +89,7 @@ void *Server_Socket_Thread(void *arg){
     close(sock);
 
     return 0;
-}
+}*/
 
 void *UDP_Thread(void *args){
 
@@ -127,10 +105,6 @@ void *UDP_Thread(void *args){
     printf("Client IP : %s Port : %d\n", inet_ntoa(utf.from_addr.sin_addr), ntohs(utf.from_addr.sin_port));
 
     clock_gettime(CLOCK_REALTIME, &T[0]);
-
-    for (cm = CMSG_FIRSTHDR(&utf.msg); cm; cm = CMSG_NXTHDR(&utf.msg, cm))
-            if (SOL_SOCKET == cm->cmsg_level && SO_TIMESTAMPNS == cm->cmsg_type)
-                memcpy(&T[0], (struct timespec *)CMSG_DATA(cm), sizeof(struct timespec));
 
     T_int[0] = T[0].tv_sec; T_int[1] = T[0].tv_nsec;
 
@@ -148,60 +122,19 @@ void *UDP_Thread(void *args){
 
 }
 
-int TCP_server(struct sockaddr_in *server_addr){
-
-    pthread_t p_thread; // thread identifier
-
-    struct sockaddr_in client_addr;
-
-    int sock, new, client_len = sizeof(client_addr), enabled = 1; // enabled should be 1
-
-    /* TCP */
-
-    if((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-        printf("TCP socket() failed\n");
-        exit(1);
-    }
-
-    printf("TCP socket() success\n");
-
-    /* SO_TIMESTAMPNS */
-
-    if(setsockopt(sock, SOL_SOCKET, SO_TIMESTAMPNS, &enabled, sizeof(enabled)) < 0)
-        err("setsockopt()");
-
-    if(bind(sock, (struct sockaddr*)server_addr, sizeof(*server_addr)) < 0)
-        err("bind()");
-
-    printf("bind() success\n");
-
-    if(listen(sock, BACKLOG) < 0)
-        err("listen()");
-
-    printf("listen() success\n");
-
-    while((new = accept(sock, (struct sockaddr*)&client_addr, &client_len)) != -1){
-        if(Thread_t < 0) Thread_t = 0; // additional consideration is demanded
-
-        if(pthread_create(p_thread, NULL, Server_Socket_Thread, (void *)new) == 0) Thread_t++; // thread success return 0 pthread overlap is ok but index -1 is not ok
-
-        pthread_detach(p_thread); // 자원 반납
-
-        printf("%d : Client IP : %s Port : %d\n", Thread_t, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-    }
-
-    close(sock);
-
-    return 0;
-}
-
 int UDP_server(struct sockaddr_in *server_addr){
 
     udp_thread_factor utf;
 
     pthread_t p_thread; // thread identifier
 
+    int client_len = sizeof(struct sockaddr_in);
+
     int sock, enabled = 1;
+
+    char recv_buf[DEFAULT_BUFLEN];
+
+	int recv_buflen = sizeof(recv_buf);
 
     if((utf.sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
         printf("UDP socket() failed\n");
@@ -210,15 +143,12 @@ int UDP_server(struct sockaddr_in *server_addr){
 
     printf("UDP socket() success\n");
 
-    if(setsockopt(utf.sock, SOL_SOCKET, SO_TIMESTAMPNS, &enabled, sizeof(enabled)) < 0)
-        err("setsockopt()");
-
     if(bind(utf.sock, (struct sockaddr*)server_addr, sizeof(*server_addr)) < 0)
         err("bind()");
 
     printf("bind() success\n\n");
 
-    while(recv_socket(utf.sock, &utf.msg, &utf.from_addr) > 0){
+    while(recvfrom(utf.sock, recv_buf, recv_buflen, 0, (struct sockaddr_in*)&utf.from_addr, &client_len) > 0){
 
         if(pthread_create(&p_thread, NULL, UDP_Thread, (void *)&utf) != 0)
             err("thread error");
@@ -254,11 +184,7 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
 
-    if (protocol == 0)
-		UDP_server(&server_addr);
-
-	else
-		TCP_server(&server_addr);
+	UDP_server(&server_addr);
 
     return 0;
 }
