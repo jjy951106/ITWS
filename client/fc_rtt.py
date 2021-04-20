5889e9a6ae24e2d8dea781cdf044460f04ba61a6
jjy951106 <37281676+jjy951106@users.noreply.github.com>
2021-04-20 20:58:31 +0900

    Update fc_rtt.py

--- client/fc_rtt.py
+++ client/fc_rtt.py
@@ -3,7 +3,7 @@ import paho.mqtt.client as mqtt
 from pymavlink import mavutil
 from datetime import datetime as dt
 
-HOST = "192.168.0.157"
+HOST = "192.168.0.157" # 1.239.197.74
 PORT = 5005
 ADDR = (HOST, PORT)
 
